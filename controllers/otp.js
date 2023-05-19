b65666758c89d9a22920a9834d4c843cb53d74db
Pooja Sahani <poojasahani954@gmail.com>
2023-05-19 14:11:51 +0530

    otp updated

--- controllers/otp.js
+++ controllers/otp.js
@@ -1,41 +1,43 @@
-const Otp =  require('../models/otp');
-const nodemailer = require('nodemailer');
-const User = require('../models/signup');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
+const Otp = require("../models/otp");
+const nodemailer = require("nodemailer");
+const User = require("../models/signup");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 postOtp = async (req, res, next) => {
+  try {
+    const otp = String(req.body.otp);
 
-    try {
-        
-        const otp = String(req.body.otp);
+    const userId = req.headers.authorization;
 
-        // const userId = req.headers
+    const otps = await Otp.findOne({ userId });
 
-  
-        const otps = await OTP.findOne({ userId: userId, otp : otp });
-        if(otps != null){
-
-            res.status(200).json({
-                statusCode: 200,
-                statusMessage: "OK",
-                result: {
-                    success: "OTP is matched!"
-                },
-                data: {
-                    token: token
-                }
-            })
-        }
-        else{
-
-        }
-    }
-    catch(err){
-        throw err;
-    }
-}
+    if (otps != null) {
+      const isMatch = await bcrypt.compare(otp, otps.otp);
+      if (isMatch) {
+        res.status(200).json({
+          statusCode: 200,
+          statusMessage: "OK",
+          result: {
+            success: "OTP is matched!",
+          }
+        });
+      }
+      else {
+        res.status(200).json({
+          statusCode: 200,
+          statusMessage: "OK",
+          result: {
+            success: "OTP not matched!",
+          }
+        });
+      }
+    } 
+  } catch (err) {
+    throw err;
+  }
+};
 
 module.exports = {
-    postOtp
-}
+  postOtp,
+};
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log("connection successfull");
-    });
-const signupRoutes = require('./routes/signup');
-const signinRoutes = require('./routes/signin');
+const mongoose = require("mongoose");
 
-app.use('/', signupRoutes);
+const signupRoutes = require("./routes/signup");
+const signinRoutes = require("./routes/signin");
+
+app.use("/", signupRoutes);
 app.use(signinRoutes);
 
-app.listen(8000);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection successfull");
+    app.listen(8000);
+  });
